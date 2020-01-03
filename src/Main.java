3b5fcd6bd4c8542354f24c433ee2a03f66680347
AlmAtoi <atoimbekov@gmail.com>
2020-01-03 16:13:10 +0600

    Improved tags parsing logic

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,25 @@
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
+        boolean readFromComandLine = args.length != 0;
         if (args.length != 2){
             System.out.println("There must be 2 arguments: path to source file and new file's name");
         }
-//        String[] textArr = FileInteraction.getFileTextArr("./file.txt");
-        String[] textArr = FileInteraction.getFileTextArr(args[0]);
+
+        String[] textArr;
+        if (readFromComandLine) textArr = FileInteraction.getFileTextArr(args[0]);
+        else textArr = FileInteraction.getFileTextArr("src/file.txt");
+
+        // Building tags tree
         TagTree tagTree = new TagTree(textArr);
 
+        // Parsing tags tree into string result
         StringBuilder sb = new StringBuilder();
-        inOrderTraverse(tagTree.tagsTree, sb);
+        Parsing.inOrderTraverse(tagTree.tagsTree, sb);
 
-        System.out.println(sb.toString());
-//        FileInteraction.writeToFile("./output.txt", sb.toString());
-        FileInteraction.writeToFile(args[1], sb.toString());
-    }
+        // Result output
+        if (readFromComandLine) FileInteraction.writeToFile(args[1], sb.toString());
+        else System.out.println(sb.toString());
 
-    static void inOrderTraverse(List<TagLevel> tagLevels, StringBuilder sb) {
-        for (int i = 0; i < tagLevels.size(); i++) {
-            TagLevel currentTag = tagLevels.get(i);
-            int delimiter = getFirstWordDelim(currentTag.body);
-            String tag = currentTag.body.substring(0, delimiter);
-            String rest = currentTag.body.substring(delimiter);
-
-            sb.append("<");
-            sb.append(tag);
-            sb.append(">");
-            if (!rest.equals(" ")) sb.append(rest);
-            sb.append("\n");
-
-            inOrderTraverse(tagLevels.get(i).children, sb);
-            sb.append("</");
-            sb.append(tag);
-            sb.append(">");
-            sb.append("\n");
-        }
-    }
-
-    static int getFirstWordDelim(String string) {
-        int count = 0;
-        boolean wordStarted = false;
-        for (int i = 0; i < string.length(); i++) {
-            if (!wordStarted && string.charAt(i) != ' ') wordStarted = true;
-            else if(wordStarted) {
-                char delim = string.charAt(i);
-                if (delim == ' ' || delim == '.' || delim == '(') return count;
-            }
-            count++;
-        }
-        return count;
     }
 }
---
+++ src/Parsing.java
@@ -0,0 +1,80 @@
+import java.util.List;
+
+public class Parsing {
+    static void inOrderTraverse(List<TagLevel> tagLevels, StringBuilder sb) {
+        for (int i = 0; i < tagLevels.size(); i++) {
+            TagLevel currentTag = tagLevels.get(i);
+            String tag = currentTag.body;
+            String rest = "";
+            if (!openedParenth) {
+                int delimiter = getFirstWordDelim(currentTag.body);
+                tag = currentTag.body.substring(0, delimiter);
+                rest = currentTag.body.substring(delimiter);
+            }
+
+            parseOpeningTags(sb, tag, rest);
+
+            inOrderTraverse(tagLevels.get(i).children, sb);
+
+            if (!openedParenth && !rest.equals("")) parseClosingTags(sb, tag);
+        }
+    }
+
+    static boolean openedParenth = false;
+    static void parseOpeningTags(StringBuilder sb, String tag, String rest) {
+        if (openedParenth) {
+            parseAttributes(tag, sb);
+        } else {
+            sb.append('<');
+            if (tag.equals("doctype")) sb.append("!DOCTYPE");
+            else sb.append(tag);
+            if (rest.equals(" ")) sb.append(">\n");
+            else if (rest.charAt(0) == '(') {
+                openedParenth = true;
+                sb.append(' ');
+                parseAttributes(rest.substring(1), sb);
+            } else {
+                sb.append('>');
+                sb.append(rest, 0, rest.length() - 1);
+            }
+        }
+    }
+
+    static void parseAttributes(String tag, StringBuilder sb) {
+        for (int i = 0; i < tag.length(); i++) {
+            if (tag.charAt(i) == '(') System.out.println("Unexpected text '(");
+            else if (tag.charAt(i) == '\'') sb.append('"');
+            else if (tag.charAt(i) == ')') {
+                openedParenth = false;
+                sb.append('>');
+
+                String rest = tag.substring(i + 1);
+                if (rest.charAt(0) != ' ') System.out.println("Unexpected text '" + rest.charAt(0) + "'");
+                sb.append(' ');
+                sb.append(rest.substring(1));
+                sb.append('\n');
+                return;
+            }
+            else sb.append(tag.charAt(i));
+        }
+    }
+
+    static void parseClosingTags(StringBuilder sb, String tag) {
+        if (!tag.equals("doctype")) {
+            sb.append("</");
+            sb.append(tag);
+            sb.append(">");
+        }
+        sb.append("\n");
+    }
+
+    static int getFirstWordDelim(String string) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char delim = string.charAt(i);
+            if (delim == ' ' || delim == '.' || delim == '(' || delim == ')') return count;
+            count++;
+        }
+        return count;
+    }
+}
